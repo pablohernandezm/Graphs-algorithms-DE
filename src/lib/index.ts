5c4cbafc0b9787982d83b09c67719f44ab6ecfa9
Pablo Hernández <phernandezm07@gmail.com>
2023-12-11 23:53:42 -0500

    Entrega-01

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -49,3 +49,153 @@ export function fixPointsToRadius(start:Point, end:Point){
 
     return {start, end}
 }
+
+export function fordFulkerson(graphNodes: GraphNode[], graphLines: GraphLine[]): number {
+    const sourceNode = graphNodes.find(node => node.isSource);
+    const sinkNode = graphNodes.find(node => node.isSink);
+
+    if (!sourceNode || !sinkNode) {
+        console.error('No se encontró el nodo fuente o sumidero.');
+        return 0;
+    }
+
+    const source = graphNodes.indexOf(sourceNode);
+    const sink = graphNodes.indexOf(sinkNode);
+
+    const residualGraph = initializeResidualGraph(graphLines);
+    let maxFlow = 0;
+
+    while (graphNodes.length>0) {
+        const { path, minCapacity } = findAugmentingPath(residualGraph, source, sink);
+
+        if (!path) {
+            break;
+        }
+
+        for (let i = 0; i < path.length - 1; i++) {
+            const node1 = path[i];
+            const node2 = path[i + 1];
+
+            const forwardEdge = residualGraph.find(edge => edge.node1 === node1 && edge.node2 === node2);
+            const backwardEdge = residualGraph.find(edge => edge.node1 === node2 && edge.node2 === node1);
+
+            if (forwardEdge) {
+                forwardEdge.weight -= minCapacity;
+            }
+
+            if (backwardEdge) {
+                backwardEdge.weight += minCapacity;
+            } else {
+                // Si no existe la arista residual, créala
+                residualGraph.push({ node1: node2, node2: node1, weight: minCapacity, type: LineType.bidirectional });
+            }
+        }
+
+        maxFlow += minCapacity;
+    }
+
+    return maxFlow;
+}
+
+function initializeResidualGraph(graphLines: GraphLine[]): GraphLine[] {
+    const residualGraph: GraphLine[] = [];
+
+    for (const edge of graphLines) {
+        residualGraph.push({ ...edge });
+        // Asegúrate de agregar también las aristas residuales iniciales
+        residualGraph.push({ node1: edge.node2, node2: edge.node1, weight: 0, type: LineType.bidirectional });
+    }
+
+    return residualGraph;
+}
+
+function findAugmentingPath(residualGraph: GraphLine[], source: number, sink: number): { path: number[] | null, minCapacity: number } {
+    const visited = Array(residualGraph.length).fill(false);
+    const queue = [source];
+    const parent: Record<number, number> = {};
+    let minCapacity = Number.POSITIVE_INFINITY;
+
+    while (queue.length > 0) {
+        const currentNode = queue.shift() as number;
+        visited[currentNode] = true;
+
+        for (const edge of residualGraph) {
+            if (edge.node1 === currentNode && !visited[edge.node2] && edge.weight > 0) {
+                queue.push(edge.node2);
+                parent[edge.node2] = currentNode;
+                minCapacity = Math.min(minCapacity, edge.weight);
+
+                if (edge.node2 === sink) {
+                    // Hemos alcanzado el nodo de destino, construimos el camino.
+                    const path = reconstructPath(parent, source, sink);
+                    return { path, minCapacity };
+                }
+            }
+        }
+    }
+
+    return { path: null, minCapacity: 0 };
+}
+
+function reconstructPath(parent: Record<number, number>, source: number, sink: number): number[] {
+    const path: number[] = [];
+    let currentNode = sink;
+
+    while (currentNode !== source) {
+        path.unshift(currentNode);
+        currentNode = parent[currentNode];
+    }
+
+    path.unshift(source);
+    return path;
+}
+
+
+
+
+export function dijkstra(graphNodes: GraphNode[], graphLines: GraphLine[], startNodeIndex: number): { distances: number[], previousNodes: number[] } {
+    const n = graphNodes.length;
+
+    const distances: number[] = Array(n).fill(Number.POSITIVE_INFINITY);
+    const previousNodes: number[] = Array(n).fill(-1); // Inicializa todos los nodos anteriores como -1
+    const visited: boolean[] = Array(n).fill(false);
+
+    distances[startNodeIndex] = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        const minDistNode = minDistance(distances, visited);
+
+        visited[minDistNode] = true;
+
+        for (let j = 0; j < n; j++) {
+            console.log("hola1")
+            if (!visited[j] && graphLines.some(line => (line.node1 === minDistNode && line.node2 === j) || (line.node1 === j && line.node2 === minDistNode))) {
+                const weight = graphLines.find(line => (line.node1 === minDistNode && line.node2 === j) || (line.node1 === j && line.node2 === minDistNode))?.weight || 0;
+                const totalDist = distances[minDistNode] + weight;
+                console.log(j)
+
+                if (totalDist < distances[j]) {
+                    distances[j] = totalDist;
+                    console.log(j)
+                    previousNodes[j] = minDistNode;
+                }
+            }
+        }
+    }
+
+    return { distances, previousNodes };
+}
+
+function minDistance(dist: number[], visited: boolean[]): number {
+    let min = Number.POSITIVE_INFINITY;
+    let minIndex = -1;
+
+    for (let i = 0; i < dist.length; i++) {
+        if (!visited[i] && dist[i] <= min) {
+            min = dist[i];
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+}
--- src/routes/aside.svelte
+++ src/routes/aside.svelte
@@ -1,5 +1,63 @@
-<script>
+<script lang="ts">
     import {Play} from "lucide-svelte";
+    import { dijkstra, fordFulkerson } from '$lib';
+    import { graphNodes, graphLines } from '$lib/stores';
+
+    let fordFlow:number
+    let dijkstraPath:number[]=[]
+
+    $:console.info(dijkstraPath);
+    $:console.info(fordFlow);
+
+    function runFordFulkerson() {
+        if($graphNodes.length===0) return;
+
+        fordFlow = fordFulkerson($graphNodes, $graphLines);
+
+        console.log('Flujo máximo:', fordFlow);
+    }
+
+    function runDijkstra() {
+        const startNodeIndex = 0;
+        const { distances, previousNodes } = dijkstra($graphNodes, $graphLines, startNodeIndex);
+
+        dijkstraPath = [];
+
+        const sourceNodes = $graphNodes.filter(node => node.isSource);
+
+        const sinkNodes = $graphNodes.filter(node => node.isSink);
+
+        let destinationNodeIndex = -1;
+        let minDistance = Infinity;
+
+        sourceNodes.forEach(()=>{
+            for (const sinkNode of sinkNodes) {
+                const sinkIndex = $graphNodes.indexOf(sinkNode);
+                if (distances[sinkIndex] < minDistance) {
+                    minDistance = distances[sinkIndex];
+                    destinationNodeIndex = sinkIndex;
+                }
+            }
+        })
+
+        if (destinationNodeIndex !== -1) {
+            let currentNode = destinationNodeIndex;
+
+            dijkstraPath.unshift(currentNode);
+
+            while (currentNode !== startNodeIndex && currentNode !== -1) {
+                currentNode = previousNodes[currentNode];
+
+                if (currentNode !== startNodeIndex && !dijkstraPath.includes(currentNode)) {
+                    dijkstraPath.unshift(currentNode);
+                }
+            }
+
+            if (!dijkstraPath.includes(startNodeIndex)) {
+                dijkstraPath.unshift(startNodeIndex);
+            }
+        }
+    }
 </script>
 <aside class="flex flex-col w-full md:max-w-[300px] h-full bg-base-300 rounded-sm p-4 justify-start gap-8 border-r-2 border-r-gray-500">
     <div class="flex flex-col gap-4">
@@ -7,14 +65,14 @@
 
         <div>
             <div>
-                <button class="btn btn-ghost text-xl w-full justify-start flex-nowrap">
+                <button class="btn btn-ghost text-xl w-full justify-start flex-nowrap" on:click={runDijkstra}>
                     <Play class="flex-shrink-0"/>
                     <span class="line-clamp-1 text-start">Dijkstra</span>
                 </button>
             </div>
 
             <div>
-                <button class="btn btn-ghost text-xl w-full justify-start flex-nowrap">
+                <button class="btn btn-ghost text-xl w-full justify-start flex-nowrap" on:click={runFordFulkerson}>
                     <Play class="flex-shrink-0"/>
                     <span class="line-clamp-1 text-start">Ford Fulkerson</span>
                 </button>
@@ -27,11 +85,26 @@
             Resultados
         </h3>
 
-        <ul class="steps md:steps-vertical justify-center items-center flex md:flex-col">
-            <li class="md:grid-cols-[1fr_0] step step-neutral w-0 " data-content="S"/>
-            <li class="md:grid-cols-[1fr_0] step step-neutral w-0 " data-content="N2"/>
-            <li class="md:grid-cols-[1fr_0] step step-neutral w-0 " data-content="N3"/>
-            <li class="md:grid-cols-[1fr_0] step step-neutral w-0 " data-content="T"/>
-        </ul>
+        {#if dijkstraPath.length>0}
+            <h4 class="font-bold text-xl">
+                Camino más corto
+            </h4>
+
+            <ul class="steps md:steps-vertical justify-center items-center flex md:flex-col">
+                {#each dijkstraPath as idx}
+                    {#if $graphNodes[idx]}
+                        <li class="md:grid-cols-[1fr_0] step step-neutral w-0 " data-content={$graphNodes[idx].isSource?'S':$graphNodes[idx].isSink?'T':`N${idx}`}/>
+                    {/if}
+                {/each}
+            </ul>
+        {/if}
+
+        {#if fordFlow}
+            <h4 class="font-bold text-xl">
+                Flujo máximo
+            </h4>
+
+            {fordFlow}
+        {/if}
     </div>
 </aside>
